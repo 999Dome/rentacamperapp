d693dc2c47c776e983bbb1a051193da8ea1692da
999Dome <124059485+999Dome@users.noreply.github.com>
2026-05-19 16:15:28 +0200

    added base structure for further developmet

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'aboutus/index.html'),
+        account: resolve(__dirname, 'account/index.html'),
+        rent: resolve(__dirname, 'rent/index.html'),
+        rentout: resolve(__dirname, 'rentout/index.html')
+      },
+    },
+  },
+})
