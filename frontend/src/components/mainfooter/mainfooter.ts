4e454f84ad7e00ea94c95fe048b614556806805e
999Dome <124059485+999Dome@users.noreply.github.com>
2026-06-09 13:54:03 +0200

    feat: Created Hompeage Design

--- frontend/src/components/mainfooter/mainfooter.ts
+++ frontend/src/components/mainfooter/mainfooter.ts
@@ -1,24 +1,10 @@
+import footerHTML from "./mainfooter.html?raw";
+
 class mainFooter extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
-
-  async connectedCallback() {
-    const response = await fetch("./mainfooter.html");
-    const text = await response.text();
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(text, "text/html");
-
-    while (doc.body.firstChild) {
-      this.shadowRoot!.appendChild(doc.body.firstChild);
-    }
-
-    while (doc.head.firstChild) {
-      this.shadowRoot!.appendChild(doc.head.firstChild);
-    }
+  connectedCallback() {
+    this.innerHTML = footerHTML;
   }
 }
 
 customElements.define("main-footer", mainFooter);
+
